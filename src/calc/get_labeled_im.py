95214446907ebccfbe0974a14dd2e95230b0f5aa
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-12-11 10:46:37 -0500

    clean

--- src/calc/get_labeled_im.py
+++ src/calc/get_labeled_im.py
@@ -10,17 +10,16 @@ def get_labeled_im(pred_f):
     l = pred_data[:, 2]  # lymph
     n = pred_data[:, 3]  # necrosis
 
-    calc_width = x.min() + x.max()
-    patch_size = calc_width / len(np.unique(x))
+    # calc_width = x.min() + x.max()   # var not used
+    # calc_height = y.min() + y.max()  # var not used
+    patch_size = (x.min() + x.max()) / len(np.unique(x))
 
     x = np.round((x + patch_size / 2.0) / patch_size)
     y = np.round((y + patch_size / 2.0) / patch_size)
 
-    maxx = int(x.max())
-    maxy = int(y.max())
     # Initialize matrices
-    iml = np.zeros((maxx, maxy), dtype=np.float32)  # img matrix lymph
-    imn = np.zeros((maxx, maxy), dtype=np.float32)  # img matrix necrosis
+    iml = np.zeros((int(x.max()), int(y.max())), dtype=np.float32)  # img matrix lymph
+    imn = np.zeros((int(x.max()), int(y.max())), dtype=np.float32)  # img matrix necrosis
 
     # Populate matrices
     for iter in range(len(x)):
