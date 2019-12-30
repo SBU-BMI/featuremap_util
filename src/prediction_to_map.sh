6381ccab83cab14c0abd8be3e847b97ffccede53
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-12-30 15:33:54 -0500

    error check

--- src/prediction_to_map.sh
+++ src/prediction_to_map.sh
@@ -39,35 +39,37 @@ for f in $color_files; do
   break
 done
 
-# We get the slides based on what's in this heatmap_txt folder
-for files in $HEAT_LOC/color-*; do
+if [[ $found -eq 0 ]]; then
+  # We get the slides based on what's in this heatmap_txt folder
+  for files in $HEAT_LOC/color-*; do
 
-  # From the color- file name, deduce the matching slide name (minus the extension)
-  SVS=$(echo ${files} | awk -F'/' '{print $NF}' | awk -F'color-' '{print $2}')
+    # From the color- file name, deduce the matching slide name (minus the extension)
+    SVS=$(echo ${files} | awk -F'/' '{print $NF}' | awk -F'color-' '{print $2}')
 
-  # Find the unmodified heatmap
-  PRED=$(ls -1 ${HEAT_LOC}/prediction-${SVS}* | grep -v low_res)
-  COLOR=${files}
+    # Find the unmodified heatmap
+    PRED=$(ls -1 ${HEAT_LOC}/prediction-${SVS}* | grep -v low_res)
+    COLOR=${files}
 
-  # Find the slide
-  FILE="$(ls -1 ${SLIDES}/${SVS}*.$ext)"
-  if [ -f "$FILE" ]; then
-    SVS_FILE=$(ls -1 ${SLIDES}/${SVS}*.$ext | head -n 1)
-  fi
+    # Find the slide
+    FILE="$(ls -1 ${SLIDES}/${SVS}*.$ext)"
+    if [ -f "$FILE" ]; then
+      SVS_FILE=$(ls -1 ${SLIDES}/${SVS}*.$ext | head -n 1)
+    fi
 
-  if [ -z "$SVS_FILE" ]; then
-    echo "Could not find slide."
-    continue
-  fi
+    if [ -z "$SVS_FILE" ]; then
+      echo "Could not find slide."
+      continue
+    fi
 
-  # Get width and height
-  WIDTH=$(openslide-show-properties ${SVS_FILE} |
-    grep "openslide.level\[0\].width" | awk '{print substr($2,2,length($2)-2);}')
-  HEIGHT=$(openslide-show-properties ${SVS_FILE} |
-    grep "openslide.level\[0\].height" | awk '{print substr($2,2,length($2)-2);}')
+    # Get width and height
+    WIDTH=$(openslide-show-properties ${SVS_FILE} |
+      grep "openslide.level\[0\].width" | awk '{print substr($2,2,length($2)-2);}')
+    HEIGHT=$(openslide-show-properties ${SVS_FILE} |
+      grep "openslide.level\[0\].height" | awk '{print substr($2,2,length($2)-2);}')
 
-  # Generate CSVs and PNGs.
-  python "$(pwd)/prediction_to_map.py" ${SVS} ${WIDTH} ${HEIGHT} ${PRED} ${COLOR} ${output_dir} ${exec_id} ${exec_by}
-done
+    # Generate CSVs and PNGs.
+    python "$(pwd)/prediction_to_map.py" ${SVS} ${WIDTH} ${HEIGHT} ${PRED} ${COLOR} ${output_dir} ${exec_id} ${exec_by}
+  done
+fi
 
 exit 0
