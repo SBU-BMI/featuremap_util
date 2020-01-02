4b14fa91c310d5964be6ca834b371b66e75ef5ae
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-01-02 14:33:03 -0500

    cherry-pick prediction_to_map from...
    `feat_multi_class`

--- src/prediction_to_map.sh
+++ src/prediction_to_map.sh
@@ -27,6 +27,8 @@ executionid="$5"
 executedby="$6"
 found=0
 
+echo "args: $1 $2 $3 $4 $5 $6"
+
 color_files="$HEAT_LOC/color-*"
 for f in $color_files; do
 
@@ -40,6 +42,35 @@ for f in $color_files; do
 done
 
 if [[ $found -eq 0 ]]; then
+
+  for files in $HEAT_LOC/prediction-*; do
+
+    # From the prediction- file name, deduce the matching slide name (minus the extension)
+    SVS=$(echo ${files} | awk -F'/' '{print $NF}' | awk -F'prediction-' '{print $2}')
+
+    # Find the slide
+    FILE="$(ls -1 ${SLIDES}/${SVS}*.$ext)"
+    if [ -f "$FILE" ]; then
+      SVS_FILE=$(ls -1 ${SLIDES}/${SVS}*.$ext | head -n 1)
+    fi
+
+    if [ -z "$SVS_FILE" ]; then
+      echo "Could not find slide."
+      continue
+    fi
+
+    # Get width and height
+    WIDTH=$(openslide-show-properties ${SVS_FILE} |
+      grep "openslide.level\[0\].width" | awk '{print substr($2,2,length($2)-2);}')
+    HEIGHT=$(openslide-show-properties ${SVS_FILE} |
+      grep "openslide.level\[0\].height" | awk '{print substr($2,2,length($2)-2);}')
+
+    # Generate CSVs and PNGs.
+    # python "$(pwd)/prediction_to_map.py" ${SVS} ${WIDTH} ${HEIGHT} ${files} "dummy" ${output_dir} ${executionid} ${executedby}
+    python "$(pwd)/prediction_to_map.py" $SVS_FILE $WIDTH $HEIGHT $files "dummy" $output_dir $executionid $executedby
+
+  done
+else
   # We get the slides based on what's in this heatmap_txt folder
   for files in $HEAT_LOC/color-*; do
 
