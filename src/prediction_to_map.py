6baa1ac863e19ddde3bb46e3090fb050b646a885
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2020-01-08 09:43:39 -0500

    catch exception

--- src/prediction_to_map.py
+++ src/prediction_to_map.py
@@ -9,10 +9,10 @@ from calc.get_tissue_map import get_tissue_map
 from calc.get_wbr_im import get_wbr_im
 from calc.featuremap import write_map_from_matrix
 
-
 base = os.path.basename(__file__)
 if len(sys.argv) != 9:
-    print('\nUsage:\n    python3.7 ' + base + ' svs_name width height pred_file color_file output_dir executionid executedby')
+    print("Usage:")
+    print("python3.7 ' + base + ' svs_name width height pred_file color_file output_dir executionid executedby")
     exit(1)
 
 svs_name = sys.argv[1]
@@ -24,20 +24,27 @@ output_dir = sys.argv[6]
 executionid = sys.argv[7]
 executedby = sys.argv[8]
 
-# Get data from files
-pred, necr, patch_size = get_labeled_im(pred_file)
-whiteness, blackness, redness = get_wbr_im(color_file)
+try:
+    # Get data from files
+    pred, necr, patch_size = get_labeled_im(pred_file)
+    whiteness, blackness, redness = get_wbr_im(color_file)
 
-# Initialize m x n x c matrix
-im = np.zeros((pred.shape[0], pred.shape[1], 3), dtype=np.uint8)
+    # Initialize m x n x c matrix
+    im = np.zeros((pred.shape[0], pred.shape[1], 3), dtype=np.uint8)
 
-# Populate matrix
-im[:, :, 0] = 255 * pred * (blackness > 30).astype(np.uint8) * (redness < 0.15).astype(np.uint8)  # Red channel
-im[:, :, 1] = 255 * necr  # Green channel
-im[:, :, 2] = 255 * get_tissue_map(whiteness)  # Blue channel
+    # Populate matrix
+    im[:, :, 0] = 255 * pred * (blackness > 30).astype(np.uint8) * (redness < 0.15).astype(np.uint8)  # Red channel
+    im[:, :, 1] = 255 * necr  # Green channel
+    im[:, :, 2] = 255 * get_tissue_map(whiteness)  # Blue channel
 
-im = np.swapaxes(im, 0, 1)  # Transpose
+    im = np.swapaxes(im, 0, 1)  # Transpose
 
-filename = output_dir + '/{}.png'.format(svs_name)
-# imageio.imwrite(filename, im)
-write_map_from_matrix(im, [width, height], filename, executionid, executedby, False)
+    filename = output_dir + '/{}.png'.format(svs_name)
+    # imageio.imwrite(filename, im)
+    write_map_from_matrix(im, [width, height], filename, executionid, executedby, False)
+
+except ValueError as err:
+    print(pred_file)
+    print("ValueError: {0}".format(err))
+except:
+    print("Error:", sys.exc_info()[0])
